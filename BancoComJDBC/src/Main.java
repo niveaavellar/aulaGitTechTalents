b6d2503130b87ef8f8bf0a7d27c3bccbdafc28a1
Daniel Lopes <nia2ssurreall@gmail.com>
2021-03-06 11:12:32 -0300

    "adicionando conteudo"

--- BancoComJDBC/src/Main.java
+++ BancoComJDBC/src/Main.java
@@ -13,30 +13,7 @@ public class Main {
 		
 		Connection connection = DatabaseConnection.connect();
 		
-//		ClienteDao clienteDao = new ClienteDao(connection);
-//		ContaDao contaDao = new ContaDao(connection);
-//		Cliente cliente = clienteDao.get(3);
-////		System.out.println(cliente);		
-////		cliente.setEmail("nivea.avellar@gmail.com");
-////		clienteDao.update(2, cliente);
-//		
-////		Cliente clienteInsert = new Cliente();
-////		clienteInsert.setCpf("12345678912");
-////		clienteInsert.setEmail("noiadaperifa@gmail.com");
-////		clienteInsert.setNome("Noia");
-////		clienteInsert.setRenda(50f);
-////		clienteInsert.setRg("222222222");
-////		clienteInsert.setTelefone("19987777777");
-////		clienteDao.save(clienteInsert);
-//		
-//
-////		clienteDao.delete(5);
-//		
-////		Conta contaGet = contaDao.get("1000");
-////		System.out.println(contaGet);	
-//
-//		
-//		
+
 		Regras regras = new Regras(connection);
 		
 		Conta conta = regras.criarContaCorrente("Leinad", "99977755566", "4419283666", 100000f, "1991827366", "emaildoleniad@mail.com", "4422");
